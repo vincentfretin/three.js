aedec15d4c412075c635775b7623614dbd93a265
netpro2k <netpro2k@gmail.com>
2021-10-18 16:40:37 -0700

    [HUBS] Add support to use a LUT for tone mapping
    We would like a workflow for creating assets in Blender that closely
    match the results when rendered in Hubs. Part of that is matching the
    color grading. The best way to do this is to actually apply the exact
    same color grading as is applied in blender, via a LUT exported from the
    same OpenColorIO profiles.
    
    This may also prove generally useful to scene creators wishing to use a
    custom LUT for artistic reasons. This may make sense to try and land upstream.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1752,6 +1752,7 @@ function WebGLRenderer( parameters = {} ) {
 		if ( refreshMaterial ) {
 
 			p_uniforms.setValue( _gl, 'toneMappingExposure', _this.toneMappingExposure );
+			p_uniforms.setValue( _gl, 'tonemappingLUT', _this.tonemappingLUT, textures );
 
 			if ( materialProperties.needsLights ) {
 
