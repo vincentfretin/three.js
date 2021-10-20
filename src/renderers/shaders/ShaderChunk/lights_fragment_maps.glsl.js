db1aae69cf5ab77a9714a5ae1fe927588ca92fb1
netpro2k <netpro2k@gmail.com>
2021-10-20 16:08:21 -0700

    [HUBS] Don't apply IBL irradiance to lightmapped objects
    Based on our art pipeline we assume this information will already be
    baked into lightmaps. This prevents applying this indirect lighting an
    additional time, leading to objects being brighter than they should be.
    A more complete implementation would let the user configure what
    lights (and indirect light sources) should be considered "baked in" to a
    lightmap. This would be part of a larger "light linking" strategy, but
    is more than we need right now.

--- src/renderers/shaders/ShaderChunk/lights_fragment_maps.glsl.js
+++ src/renderers/shaders/ShaderChunk/lights_fragment_maps.glsl.js
@@ -10,7 +10,7 @@ export default /* glsl */`
 
 	#endif
 
-	#if defined( USE_ENVMAP ) && defined( ENVMAP_TYPE_CUBE_UV )
+	#if defined( USE_ENVMAP ) && defined( ENVMAP_TYPE_CUBE_UV ) && !defined(USE_LIGHTMAP)
 
 		#if defined( STANDARD ) || defined( LAMBERT ) || defined( PHONG )
 
